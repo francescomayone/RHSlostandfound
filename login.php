2982e6d90abb7aabddddccacbd01933921a69340
francescomayone <94423483+francescomayone@users.noreply.github.com>
2021-11-17 17:40:02 +1300

    Initial commit

---
+++ login.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<form id="login-form" method="POST" action="">
+    <h3>Have an account?</h3>
+    <input class="form-field" name="email" type="email" placeholder="Email" required>
+    <input class="form-field" name="password" type="password" placeholder="Password" required>
+    <input class="form-field form-submit" type="submit" name="login" id="loginsubmit" value="Sign In">
+    <a id="register-link" href="register.php">Don't have an account? Register now!</a>
+</form> 
---
+++ register.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<form id="login-form" method="POST">
+    <h3>New Account?</h3>
+    <input class="form-field" name="email" type="email" placeholder="Email">
+    <input class="form-field" name="password" type="password" placeholder="Password">
+    <input class="form-field" name="confirmpassword" type="password" placeholder="Confirm Password">
+
+    <input class="form-field half-form-field" name="fname" type="text" placeholder="First Name">
+    <input class="form-field half-form-field float-right" name="lname" type="text" placeholder="Last Name">
+    <input class="form-field form-submit" type="submit" name="registersubmit" id="registersubmit" value="Register">
+    <a id="register-link" href="login.php">Already have an account? Log in here!</a>
+</form>
